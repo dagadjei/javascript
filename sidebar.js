a83d5caa13a801d08d291ea7f5cb2075a5f7544f
unknown <adjeidag@gmail.com>
2021-11-03 23:11:25 +0000

    new

--- sidebar.js
+++ sidebar.js
@@ -5,3 +5,7 @@ let closeBtn = document.querySelector(".closeBtn")
 toggleBtn.addEventListener("click", function(){
     links.classList.toggle("show-sidebar")
 })
+
+closeBtn.addEventListener("click", function(){
+    links.classList.remove("show-sidebar")
+})
