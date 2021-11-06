c5570c5ea61c567748dd2fea9fe4915f2380a984
unknown <adjeidag@gmail.com>
2021-11-06 13:43:22 +0000

    new

--- sidebar.js
+++ sidebar.js
@@ -6,3 +6,6 @@ toggleBtn.addEventListener("click", function(){
     links.classList.toggle("show-sidebar")
 })
 
+closeBtn.addEventListener("click", function(){
+    links.classList.remove("show-sidebar")
+})
