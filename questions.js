b5addadc2f4e210636298c28b7583147e7ee0bbf
unknown <adjeidag@gmail.com>
2021-11-10 22:18:19 +0000

    new

--- questions.js
+++
@@ -1,8 +0,0 @@
-const btnEl = document.querySelectorAll(".question-btn")
-
-btnEl.forEach(function(btn){
-    btn.addEventListener("click", function(e){
-        const log = e.currentTarget.parentElement.parentElement
-        log.classList.toggle("show-text")
-    })
-})
